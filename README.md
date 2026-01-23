7cb7108a26c46fd03075566122cd103859ebf913
ipere <iperederin@icloud.com>
2026-01-23 21:02:26 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Granular MD
+
+k - scaling system factor  
+i - maximum number of iterations  
+m - step of logging  
+
+Small approximation
+```
+python heating_aurum.py -k 2 -i 10000 -m 50 -l 250
+```  
+
+Great approximation:  
+```
+python heating_aurum.py -k 4 -i 10000 -m 5 -l 1
+```
--- heating_aurum.py
+++ heating_aurum.py
@@ -1,11 +1,8 @@
-from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
-from ase.md import VelocityVerlet
 from ase.build import bulk
 from ase.calculators.lj import LennardJones
 from ase.io import write
 import numpy as np
 from ase import units
-from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 import matplotlib.pyplot as plt
 from datetime import date
 from ase import Atoms
@@ -22,6 +19,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-k', '--scale')
 parser.add_argument('-i', '--iteration')
 parser.add_argument('-m', '--measure_frequency')
+parser.add_argument('-l', '--log_interval')
 
 args = parser.parse_args()
 
@@ -29,45 +27,51 @@ K = int(args.scale)
 iteration = int(args.iteration)
 
 measure_frequency = int(args.measure_frequency)
+log_interval = int(args.log_interval)
 
-
-NUMBER_OF_ATOMS= 16 // K # Scaling the size!!! Basic number of atoms is 16.
+BASIC_NUMBER_ATOMS = 8
+NUMBER_OF_ATOMS= BASIC_NUMBER_ATOMS // K # Scaling the size!!! Basic number of atoms is 16.
 
 
 sigma_for_gold = 4.08 * (3.6/2.33)**(-1)
 SIGMA= K * sigma_for_gold # Scaling for sigma
 
 ATOMIC_UNIT_MASS = 196.196 * (K**3) # Scaling for masses
-EPSILON=0.4 # * (K**3) # Scaling for masses
-n_layers = 5
+EPSILON=0.4 * (K**3) # Scaling for masses
+n_layers = NUMBER_OF_ATOMS
 
 
-A = SIGMA * (3.6/2.33) # Scaling for laticce constant had already done
+A = SIGMA * (3.6/2.33)
 
 au = bulk("Au", "fcc", a=A, cubic=True)
 
 cube = au.repeat((NUMBER_OF_ATOMS, NUMBER_OF_ATOMS, NUMBER_OF_ATOMS))
+
 cube.pbc = (True, True, False)
+
+print(f"Для K={K} vs K=1:")
+print(f"Объём: {(A ** 3) * (NUMBER_OF_ATOMS**3)} == {(4.08 ** 3)* (BASIC_NUMBER_ATOMS**3)}")
+print(f"Масса: {len(cube) * ATOMIC_UNIT_MASS}  ==  {((BASIC_NUMBER_ATOMS)**3)*4 * 196.196}")
+print(f"Количество атомов (должно отличаться): {len(cube)} != {((BASIC_NUMBER_ATOMS)**3)*4}")
 
 write("init_cube.xyz", cube)
 
 cube.calc = (
     LennardJones(sigma=SIGMA, epsilon=EPSILON)
 )
-layer_thikness = A / 2
+layer_thikness = A
 
 cube.set_masses(np.repeat([ATOMIC_UNIT_MASS], len(cube.get_masses()))) # Setting masses of the atoms
 
 pos = cube.get_positions()
-
 all_z = pos[:, 2]
 min_z = np.min(all_z)
+
 
 MASKS_OF_LAYERS = [
     (all_z >= min_z + i*layer_thikness) & (all_z < min_z + (i+1)*layer_thikness)
     for i in range(n_layers)
 ]
-MASKS_OF_LAYERS.append(all_z >= min_z + (n_layers - 1)*layer_thikness)
 
 iteration_count = 0
 
@@ -79,6 +83,7 @@ def compute_layer_temperatures_smooth(trajectory, atoms=cube):
 
     for i, mask in enumerate(MASKS_OF_LAYERS):
         temp_slice = atoms[mask]
+
         layer_temp = temp_slice.get_temperature()  # K
         temps.append(layer_temp)
 
@@ -103,17 +108,6 @@ def plot_layer_temperatures(atoms=cube):
         temps.append(layer_temp)
         z_positions.append(min_z + i*layer_thikness)
 
-
-    plt.figure(figsize=(8, 4))
-    plt.plot(z_positions, temps, "o-", lw=2, markersize=4)
-    plt.ylim(0, max(300, max(temps) * 1.1))
-    plt.xlabel("Координата по оси Z (Å)")
-    plt.ylabel("Температура (K)")
-    plt.title("Распределение температуры по слоям")
-    plt.grid(alpha=0.3)
-    plt.tight_layout()
-    plt.show()
-
     return temps
 
 trajectory = []
@@ -126,7 +120,7 @@ thermostat = mdmd.MultiGroupLangevinMD(cube,
                                        timestep=5 * units.fs,
                                        friction=0.01 / units.fs)
 
-thermostat.attach(MDLogger(thermostat, cube, 'md.log', header=True, stress=False, peratom=True, mode="w"), interval=250)
+thermostat.attach(MDLogger(thermostat, cube, f'logs/md_{K}.log', header=True, stress=False, peratom=True, mode="w"), interval=log_interval)
 
 
 thermostat.attach(
@@ -137,8 +131,10 @@ thermostat.attach(
 today = date.today()
 curr_time = time.strftime("%H_%M")
 
+print("start")
+
 try:
     thermostat.run(iteration)
 except SystemExit:
     print()
-write(f"cube_iteration_scale_{K}_{curr_time}_day_{today.day}_.extxyz", trajectory, format="extxyz")
+write(f"records/cube_iteration_scale_{K}_{curr_time}_day_{today.day}_.extxyz", trajectory, format="extxyz")
