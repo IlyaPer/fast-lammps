32baeeb534073b55a65131fb9a9c02965d0ffd2b
Ilya <i@iperederin.ru>
2026-04-23 16:02:11 +0300

    Fixed scaling for JL potential

--- src/simulation_launch.py
+++ src/simulation_launch.py
@@ -1,9 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def compute_params_CG(scale_factor):
     sigma_for_gold = 4.08 * (3.6 / 2.33) ** (-1)
-    SIGMA = sigma_for_gold  # Scaling for sigma
+    SIGMA = sigma_for_gold * scale_factor # Scaling for masses
 
-    ATOMIC_UNIT_MASS = 196.196 * 3  # Scaling for masses
-    EPSILON = 0.4  # * (scale_factor**3) # Scaling for masses
+    ATOMIC_UNIT_MASS = 196.196 * (scale_factor**3)  # Scaling for masses
+    EPSILON = 0.4 * (scale_factor**3) # Scaling for masses
 
     A = SIGMA * (3.6 / 2.33) * scale_factor
     return SIGMA, A, EPSILON, ATOMIC_UNIT_MASS
