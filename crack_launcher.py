715cc83f6d4b0a2e43822e2ac9d53377422e74f2
Ilya <i@iperederin.ru>
2026-04-27 17:41:15 +0300

    Added correct launching script with baby mode

--- crack_launcher.py
+++ crack_launcher.py
@@ -2,10 +2,13 @@ from lammps import lammps
 import numpy as np
 import argparse
 import logging
-import src.atoms_extractor as ae
-from src.simulation_launch import compute_params_CG
-from src.monitor import MemoryProfiler, create_beautiful_plot
+from src.extractors.extractors import *
+from src.utils.approximation import compute_params_CG
+from src.utils.utils import LammpsExtractor
+from src.modifiers.changer import DynamicChanger
+from build.monitor import MemoryProfiler, create_beautiful_plot
 import os
+# from src.simulation_launch import CrackLayerAnalyzer
 
 parser = argparse.ArgumentParser(
     prog="Heating_Aurum",
@@ -14,7 +17,7 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument("-f", "--file")
-parser.add_argument("-k", "--scale")
+parser.add_argument("-s", "--scale", type=int, default=2)
 parser.add_argument("-i", "--iteration")
 parser.add_argument("-m", "--measure_frequency")
 parser.add_argument("-l", "--log_interval")
@@ -31,33 +34,41 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
-SCALE_FACTOR = int(args.scale) if args.solver == 'layer' else 1
+SCALE_FACTOR = int(args.scale)
 iteration = int(args.iteration)
 measure_frequency = int(args.measure_frequency)
 
-sigma_for_gold = 4.08 * (3.6 / 2.33) ** (-1)
-SIGMA = sigma_for_gold  # Scaling for sigma
-A = SIGMA * (3.6 / 2.33)
+sigma_for_nickel = 3.52 * (58.69 / 2.5) ** (-1)  # ~0.097
+SIGMA = sigma_for_nickel
+A = 3.52
 
 SIGMA_CG, A_CG, EPSILON_CG, ATOMIC_UNIT_MASS_CG = compute_params_CG(SCALE_FACTOR)
 
 logging.info(
     f"Sigma {SIGMA_CG}, Lattice {A_CG}, eps {EPSILON_CG}, Mass {ATOMIC_UNIT_MASS_CG}"
 )
-
+block = f'kkkk'
 
 L = lammps()
 
+
+communicator = LammpsExtractor(L)
+solver = FccCellsExtractor(communicator, A)
+dc = DynamicChanger(solver, A, A_CG, baby_mode=True)
+
 if args.solver == 'layer':
-    solver = ae.ExampleLayerExtractor()
+    pass
+    # solver = ExampleLayerExtractor()
 if args.solver == 'fcc':
-    solver = ae.FccCellsExtractor()
+    pass
+    # solver = FccCellsExtractor(L,A)
 
 # setup from user
 L.file(args.file)
 
+
 block = f"""
-pair_coeff      1 2 0.0 1.0
+pair_coeff      1 2 20 2.28
 pair_coeff      2 2 {EPSILON_CG} {SIGMA_CG}
 mass            2 {ATOMIC_UNIT_MASS_CG}
 lattice         fcc {A_CG}
@@ -66,29 +77,29 @@ lattice         fcc {A_CG}
 
 L.commands_string(block)
 
-
 logging.info("The simulation started successfully.")
 iter = 0
 
 
-# TODO add Prometheus checkup!
 
-with MemoryProfiler(name="my_script_analysis", track_objects=True, snapshot_interval=20) as profiler:
+with MemoryProfiler(name="accelerate", track_objects=True, snapshot_interval=20) as profiler:
     while iter < iteration:
         profiler.snapshot(iteration=iter, label=f"step_{iter}")
 
         L.cmd.run(measure_frequency)
 
-        solver.extract_interesting_regions(L, lattice_constant=A)
+       
+        dc.accelerate()
 
         L.command("reset_atoms id")
 
         L.command("run 0")
         iter += measure_frequency
+        logging.info(f'Current iteration: {iter}')
 
 
 import glob
 metric_files = glob.glob("logs/metrics_*.json")
 if metric_files:
     latest_file = max(metric_files, key=os.path.getctime)
-    create_beautiful_plot(latest_file, "records/memory_analysis.png")
+    create_beautiful_plot(latest_file, "logs/memory_analysis.png")
